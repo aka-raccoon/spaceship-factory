71176464c7df5802514ddc89a7fb941798a9f220
aka-raccoon <42897550+aka-raccoon@users.noreply.github.com>
2025-01-14 16:55:31 +0100

    update lazygit config

--- homes/_modules/shell/git/default.nix
+++ homes/_modules/shell/git/default.nix
@@ -77,19 +77,9 @@ in
           enable = true;
           settings = {
             gui = {
+              showIconns = true;
               nerdFontsVersion = 3;
               border = "rounded";
-              theme = {
-                activeBorderColor = [ "#5ef1ff" ];
-                inactiveBorderColor = [ "#7b8496" ];
-                searchingActiveBorderColor = [ "#ff5ef1" ];
-                optionsTextColor = [ "#3c4048" ];
-                selectedLineBgColor = [ "#3c4048" ];
-                cherryPickedCommitBgColor = [ "#3c4048" ];
-                cherryPickedCommitFgColor = [ "#ff5ea0" ];
-                unstagedChangesColor = [ "#ffbd5e" ];
-                defaultFgColor = [ "#ffffff" ];
-              };
             };
           };
         };
