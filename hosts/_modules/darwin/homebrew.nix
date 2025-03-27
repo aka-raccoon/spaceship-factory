266f71e379de2d41a862bbdac001e794e69e97d3
aka-raccoon <42897550+aka-raccoon@users.noreply.github.com>
2025-03-27 23:34:54 +0100

    remove unused homebrew apps

--- hosts/_modules/darwin/homebrew.nix
+++ hosts/_modules/darwin/homebrew.nix
@@ -2,7 +2,7 @@ _: {
   homebrew = {
     enable = true;
     onActivation = {
-      autoUpdate = false; # Don't update during rebuild
+      autoUpdate = false;
       cleanup = "zap"; # Uninstall all programs not declared
       upgrade = true;
     };
@@ -11,7 +11,7 @@ _: {
       lockfiles = false; # Don't save lockfile (since running from anywhere)
     };
     taps = [ ];
-    brews = [ "posting" ];
+    brews = [ ];
     casks = [
       "1password"
       "homerow"
@@ -20,7 +20,6 @@ _: {
     masApps = {
       "1Password For Safari" = 1569813296;
       "Ghostery Privacy Ad Blocker" = 6504861501;
-      "Vimari" = 1480933944;
     };
   };
 }
